c29ce98887aa8dd2a61b4dcf28af3d7c3264d48b
Maksymiv Viktor <viktor.maksymiv.23@pnu.edu.ua>
2025-03-19 19:15:05 +0200

    Fix Solution

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Any:
         if args in result_list:
             print("Getting from cache")
-            return result_list[args]
         else:
             print("Calculating new result")
             result_list[args] = func(*args)
-            return result_list[args]
+        return result_list[args]
     return inner
